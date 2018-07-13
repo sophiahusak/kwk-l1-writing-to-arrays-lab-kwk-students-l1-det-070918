d8752358dc387fb85f93b256782f3fa55e4f8a3d
sophiahusak <72futbolplayer@gmail.com>
2018-07-13 12:59:56 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -8,11 +8,11 @@ def change_rainbow_colors
   rainbow_colors[0]="red"
   rainbow_colors[1]="light_red"
   rainbow_colors[2]="light_yellow"
-  puts rainbow_colors
+  puts change_rainbow_colors
 end
 
 def add_colors
   rainbow_colors << "green"
   rainbow_colors << "blue"
-  puts rainbow_colors
+  puts add_colors
 end
